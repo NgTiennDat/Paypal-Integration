89ce0034e5efde8146b05dbdd6d392d99a9b3ab9
NgTiennDat <126660251+NgTiennDat@users.noreply.github.com>
2024-06-16 23:16:42 +0700

    new update

---
+++ src/main/java/com/datien/practicepaypal/PracticePaypalApplication.java
@@ -0,0 +1,13 @@
+package com.datien.practicepaypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticePaypalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticePaypalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datien/practicepaypal/PracticePaypalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datien.practicepaypal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticePaypalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
